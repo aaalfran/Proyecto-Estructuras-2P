735133b12a72454de3b3d423e988dcdbfea83239
aaalfran <aaalfran@espol.edu.ec>
2021-08-06 21:18:26 -0500

    Vista Principal y botones principales agregados

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -5,11 +5,15 @@
  */
 package tictactoe;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,27 +24,15 @@ import javafx.stage.Stage;
 public class TicTacToe extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("VistaInicio.fxml"));
+        stage.getIcons().add(new Image("/resources/icon.png"));
+        stage.setResizable(false);
+        Scene scene = new Scene(root);
+        stage.setTitle("Tic Tac Toe");
+        stage.setScene(scene);
+        stage.show();
     }
-
     /**
      * @param args the command line arguments
      */
---
+++ src/tictactoe/VistaInicioController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author aaron
+ */
+public class VistaInicioController {
+    
+     @FXML
+    private Button play;
+
+    @FXML
+    private Button Exit;
+
+    @FXML
+    void clickExit(ActionEvent event) {
+        Stage st = (Stage) Exit.getScene().getWindow();
+        st.close();
+    }
+
+    @FXML
+    void clickPlay(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("VistaJuego.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
