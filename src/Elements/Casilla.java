552e7a284f86d859da432b0b890800c42fb50e8a
pedbvasq <pedbvasq@espol.edu.ec>
2021-08-14 23:02:24 -0500

    matriz

--- src/Elements/Casilla.java
+++ src/Elements/Casilla.java
@@ -11,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.StackPane;
+import tictactoe.VistaJuegoController;
 
 /**
  *
@@ -18,18 +19,61 @@ import javafx.scene.layout.StackPane;
  */
 public class Casilla extends StackPane {
     
-    public Casilla(){
-        ImageView estado = new ImageView();
+    static int turnos = 1;
+    private ImageView estado;
+    private String link;
+
+    public Casilla() {
+        estado = new ImageView();
         estado.setFitHeight(100);
         estado.setFitWidth(100);
         setCursor(Cursor.HAND);
         getChildren().addAll(estado);
-        
-        setOnMouseClicked(event->{
-            if(event.getButton() == MouseButton.PRIMARY){
-                estado.setImage(new Image("/resources/x.png"));
+
+        setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                if (!((turnos % 2) == 0)) {
+                    link = "/resources/x.png";
+                    estado.setImage(new Image("/resources/x.png"));
+
+                }
+                if ((turnos % 2) == 0) {
+                    link = "/resources/o.png";
+                    estado.setImage(new Image("/resources/o.png"));
+
+                }
+
+                turnos += 1;
+                VistaJuegoController.llenarMatriz();
+                VistaJuegoController.utilidadTablero();
             }
-            
+             
+
         });
     }
+
+    public static int getTurnos() {
+        return turnos;
+    }
+
+    public static void setTurnos(int turnos) {
+        Casilla.turnos = turnos;
+    }
+
+    public ImageView getEstado() {
+        return estado;
+    }
+
+    public void setEstado(ImageView estado) {
+        this.estado = estado;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+;
 }
--- src/tictactoe/VistaJuegoController.java
+++ src/tictactoe/VistaJuegoController.java
@@ -6,6 +6,7 @@
 package tictactoe;
 
 import Elements.Casilla;
+import TDAs.LinkedList;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -33,19 +34,103 @@ public class VistaJuegoController implements Initializable {
     
 //Creando objetos en pantalla
     
-    private void crearMatriz(){
-        for(int i=0;i<3;i++){
-            for(int j =0;j<3;j++){
+    static int[][] arregloMatrix = new int[3][3];
+    static LinkedList<Casilla> listaCasillas = new LinkedList();
+
+
+
+//Creando objetos en pantalla
+    private void crearMatriz() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 Casilla cas = new Casilla();
-                cas.setTranslateX(j*160);
-                cas.setTranslateY(i*160);
-                cas.setLayoutX(j+260);
-                cas.setLayoutY(i+200);
+                cas.setTranslateX(j * 160);
+                cas.setTranslateY(i * 160);
+                cas.setLayoutX(j + 260);
+                cas.setLayoutY(i + 200);
                 root.getChildren().add(cas);
+                listaCasillas.addLast(cas);
             }
         }
+
     }
-    
+
+    public static void llenarMatriz() {
+        try {
+            if (listaCasillas.get(0).getLink().equals("/resources/x.png")) {
+                arregloMatrix[0][0] = 1;
+            }
+            if (listaCasillas.get(1).getLink().equals("/resources/x.png")) {
+                arregloMatrix[0][1] = 1;
+            }
+            if (listaCasillas.get(2).getLink().equals("/resources/x.png")) {
+                arregloMatrix[0][2] = 1;
+            }
+            if (listaCasillas.get(3).getLink().equals("/resources/x.png")) {
+
+                arregloMatrix[1][0] = 1;
+            }
+            if (listaCasillas.get(4).getLink().equals("/resources/x.png")) {
+                arregloMatrix[1][1] = 1;
+            }
+            if (listaCasillas.get(5).getLink().equals("/resources/x.png")) {
+                arregloMatrix[1][2] = 1;
+            }
+
+            if (listaCasillas.get(6).getLink().equals("/resources/x.png")) {
+                arregloMatrix[2][0] = 1;
+            }
+            if (listaCasillas.get(7).getLink().equals("/resources/x.png")) {
+                arregloMatrix[2][1] = 1;
+            }
+            if (listaCasillas.get(8).getLink().equals("/resources/x.png")) {
+                arregloMatrix[2][2] = 1;
+            }
+
+            if (listaCasillas.get(0).getLink().equals("/resources/o.png")) {
+                arregloMatrix[0][0] = 2;
+            }
+            if (listaCasillas.get(1).getLink().equals("/resources/o.png")) {
+                arregloMatrix[0][1] = 2;
+            }
+            if (listaCasillas.get(2).getLink().equals("/resources/o.png")) {
+                arregloMatrix[0][2] = 2;
+            }
+
+            if (listaCasillas.get(3).getLink().equals("/resources/o.png")) {
+                arregloMatrix[1][0] = 2;
+            }
+            if (listaCasillas.get(4).getLink().equals("/resources/o.png")) {
+                arregloMatrix[1][1] = 2;
+            }
+            if (listaCasillas.get(5).getLink().equals("/resources/o.png")) {
+                arregloMatrix[1][2] = 2;
+            }
+
+            if (listaCasillas.get(6).getLink().equals("/resources/o.png")) {
+                arregloMatrix[2][0] = 2;
+            }
+            if (listaCasillas.get(7).getLink().equals("/resources/o.png")) {
+                arregloMatrix[2][1] = 2;
+            }
+            if (listaCasillas.get(8).getLink().equals("/resources/o.png")) {
+                arregloMatrix[2][2] = 2;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public static void utilidadTablero() {
+        for (int i = 0; i < arregloMatrix.length; i++) {
+            for (int j = 0; j < arregloMatrix[i].length; j++) {
+                System.out.print(arregloMatrix[i][j] + " ");	// Imprime elemento 
+            }
+            System.out.println();	// Imprime salto de línea 
+        }
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         crearMatriz();
